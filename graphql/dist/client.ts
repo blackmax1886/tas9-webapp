49042a2d21c084380124c77969bd8c2042e4c307
blackmax <blackmax1886@gmail.com>
2023-02-07 23:41:32 +0900

    Feat: task & subtask schema

--- graphql/dist/client.ts
+++ graphql/dist/client.ts
@@ -17,65 +17,94 @@ export type Scalars = {
 
 export type Mutation = {
   __typename?: 'Mutation';
-  createTodo: Todo;
+  createTask: Task;
 };
 
 
-export type MutationCreateTodoArgs = {
-  input: NewTodo;
+export type MutationCreateTaskArgs = {
+  input: NewTask;
 };
 
-export type NewTodo = {
-  text: Scalars['String'];
+export type NewTask = {
+  content: Scalars['String'];
   userId: Scalars['String'];
 };
 
 export type Query = {
   __typename?: 'Query';
-  todos: Array<Todo>;
+  subtasks: Array<Subtask>;
+  tasks: Array<Task>;
 };
 
-export type Todo = {
-  __typename?: 'Todo';
+export type Subtask = {
+  __typename?: 'Subtask';
+  archived?: Maybe<Scalars['Boolean']>;
+  assigned_at?: Maybe<Scalars['String']>;
+  content?: Maybe<Scalars['String']>;
   done: Scalars['Boolean'];
+  due?: Maybe<Scalars['String']>;
   id: Scalars['ID'];
-  text: Scalars['String'];
+  name: Scalars['String'];
+  parent_task: Task;
+  priority?: Maybe<Scalars['String']>;
+};
+
+export type Task = {
+  __typename?: 'Task';
+  archived?: Maybe<Scalars['Boolean']>;
+  assigned_at?: Maybe<Scalars['String']>;
+  content?: Maybe<Scalars['String']>;
+  done: Scalars['Boolean'];
+  due?: Maybe<Scalars['String']>;
+  group?: Maybe<Scalars['String']>;
+  id: Scalars['ID'];
+  name: Scalars['String'];
+  priority?: Maybe<Scalars['String']>;
+  type: Scalars['String'];
   user: User;
 };
 
 export type User = {
   __typename?: 'User';
+  email: Scalars['String'];
   id: Scalars['ID'];
   name: Scalars['String'];
 };
 
-export type GetTodoQueryVariables = Exact<{ [key: string]: never; }>;
+export type GetTaskQueryVariables = Exact<{ [key: string]: never; }>;
 
 
-export type GetTodoQuery = { __typename?: 'Query', todos: Array<{ __typename?: 'Todo', id: string, text: string }> };
+export type GetTaskQuery = { __typename?: 'Query', tasks: Array<{ __typename?: 'Task', name: string, content?: string | null, done: boolean, user: { __typename?: 'User', name: string } }> };
 
 export type CreateTodoMutationVariables = Exact<{ [key: string]: never; }>;
 
 
-export type CreateTodoMutation = { __typename?: 'Mutation', createTodo: { __typename?: 'Todo', text: string, done: boolean, user: { __typename?: 'User', id: string } } };
+export type CreateTodoMutation = { __typename?: 'Mutation', createTask: { __typename?: 'Task', name: string, done: boolean, type: string, assigned_at?: string | null, user: { __typename?: 'User', id: string, name: string } } };
 
 
-export const GetTodoDocument = gql`
-    query getTodo {
-  todos {
-    id
-    text
+export const GetTaskDocument = gql`
+    query getTask {
+  tasks {
+    name
+    content
+    done
+    user {
+      name
+    }
   }
 }
     `;
 export const CreateTodoDocument = gql`
     mutation createTodo {
-  createTodo(input: {text: "todo", userId: "1"}) {
+  createTask(input: {content: "this is task text", userId: "2"}) {
     user {
       id
+      name
     }
-    text
+    name
     done
+    type
+    assigned_at
   }
 }
     `;
@@ -87,8 +116,8 @@ const defaultWrapper: SdkFunctionWrapper = (action, _operationName, _operationTy
 
 export function getSdk(client: GraphQLClient, withWrapper: SdkFunctionWrapper = defaultWrapper) {
   return {
-    getTodo(variables?: GetTodoQueryVariables, requestHeaders?: Dom.RequestInit["headers"]): Promise<GetTodoQuery> {
-      return withWrapper((wrappedRequestHeaders) => client.request<GetTodoQuery>(GetTodoDocument, variables, {...requestHeaders, ...wrappedRequestHeaders}), 'getTodo', 'query');
+    getTask(variables?: GetTaskQueryVariables, requestHeaders?: Dom.RequestInit["headers"]): Promise<GetTaskQuery> {
+      return withWrapper((wrappedRequestHeaders) => client.request<GetTaskQuery>(GetTaskDocument, variables, {...requestHeaders, ...wrappedRequestHeaders}), 'getTask', 'query');
     },
     createTodo(variables?: CreateTodoMutationVariables, requestHeaders?: Dom.RequestInit["headers"]): Promise<CreateTodoMutation> {
       return withWrapper((wrappedRequestHeaders) => client.request<CreateTodoMutation>(CreateTodoDocument, variables, {...requestHeaders, ...wrappedRequestHeaders}), 'createTodo', 'mutation');
