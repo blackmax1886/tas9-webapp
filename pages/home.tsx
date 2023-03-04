96a5ec32143a1e8998599f090e6f7cf20a8d888e
blackmax <blackmax1886@gmail.com>
2023-03-04 22:06:58 +0900

    Add: Top page

---
+++ pages/home.tsx
@@ -0,0 +1,28 @@
+import type { NextPage } from 'next'
+import { useQuery } from '@apollo/client'
+
+import { GetTasksDocument } from '../graphql/dist/client'
+import { GetTasksQuery } from '../graphql/dist/client'
+import { useSession } from 'next-auth/react'
+
+const Home: NextPage = () => {
+  const { data: session, status } = useSession()
+  const { data } = useQuery<GetTasksQuery>(GetTasksDocument, {
+    variables: { userId: session?.user.id },
+    skip: status === 'loading',
+  })
+  return (
+    <div style={{ margin: '0 auto', width: '1000px' }}>
+      {data?.tasks?.map((task) => (
+        <div key={task.id}>
+          <h1>{task.content}</h1>
+          <p>id:{task.id}</p>
+          <p>name:{task.name}</p>
+          <p>content:{task.content}</p>
+        </div>
+      ))}
+    </div>
+  )
+}
+
+export default Home
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,28 +1,14 @@
-import type { NextPage } from 'next'
-import { useQuery } from '@apollo/client'
+import { NextPage } from 'next'
+import { signIn } from 'next-auth/react'
 
-import { GetTasksDocument } from '../graphql/dist/client'
-import { GetTasksQuery } from '../graphql/dist/client'
-import { useSession } from 'next-auth/react'
-
-const Home: NextPage = () => {
-  const { data: session, status } = useSession()
-  const { data } = useQuery<GetTasksQuery>(GetTasksDocument, {
-    variables: { userId: session?.user.id },
-    skip: status === 'loading',
-  })
+const Top: NextPage = () => {
   return (
-    <div style={{ margin: '0 auto', width: '1000px' }}>
-      {data?.tasks?.map((task) => (
-        <div key={task.id}>
-          <h1>{task.content}</h1>
-          <p>id:{task.id}</p>
-          <p>name:{task.name}</p>
-          <p>content:{task.content}</p>
-        </div>
-      ))}
+    <div>
+      <h1>tas9</h1>
+      <p>Make your Day more efficient</p>
+      <button onClick={() => signIn()}>Sign in</button>
     </div>
   )
 }
 
-export default Home
+export default Top
