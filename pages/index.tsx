ab91c5bf445cd90cbb0e768d0e47c4f2d4f3ab4f
blackmax <blackmax1886@gmail.com>
2023-03-05 22:42:47 +0900

    Feat: redirect to home after signIn

--- pages/index.tsx
+++ pages/index.tsx
@@ -6,7 +6,9 @@ const Top: NextPage = () => {
     <div>
       <h1>tas9</h1>
       <p>Make your Day more efficient</p>
-      <button onClick={() => signIn('google')}>Sign in with Google</button>
+      <button onClick={() => signIn('google', { callbackUrl: '/home' })}>
+        Sign in with Google
+      </button>
     </div>
   )
 }
