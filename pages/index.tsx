dd61a710dea690ffd94fd1edc2f7069cc545b6da
blackmax <blackmax1886@gmail.com>
2023-03-07 00:32:54 +0900

    Style: align center

--- pages/index.tsx
+++ pages/index.tsx
@@ -6,19 +6,44 @@ const Button = css`
   background-color: #4caf50; /* Green */
   border: none;
   color: white;
-  padding: 15px 32px;
+  padding: 0.5rem;
   text-align: center;
-  text-decoration: none;
-  display: inline-block;
-  font-size: 16px;
+  font-size: 1rem;
+  border-radius: 0.25rem;
+`
+
+const card = css`
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  width: 25rem;
+  padding: 2.5rem;
+  box-sizing: border-box;
+  border: 1px solid #dadce0;
+  -webkit-border-radius: 8px;
+  border-radius: 8px;
+  text-align: center;
+`
+
+const heading = css`
+  font-family: sans-serif;
+  margin: 0px 0 -0.125rem;
+  padding: 0;
+  color: #fff;
+  text-align: center;
+  color: #202124;
+  font-family: 'Google Sans', 'Noto Sans Myanmar UI', arial, sans-serif;
+  font-size: 36px;
+  font-weight: 400;
 `
 
 const Top: NextPage & {
   isRootPage?: boolean
 } = () => {
   return (
-    <div>
-      <h1>tas9</h1>
+    <div css={card}>
+      <h1 css={heading}>tas9</h1>
       <p>Make your Day more efficient</p>
       <button
         css={Button}
