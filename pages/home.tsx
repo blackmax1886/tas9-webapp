5abe8e3422521e2e1372037d2d503f8585261eeb
blackmax <blackmax1886@gmail.com>
2023-03-12 00:38:42 +0900

    Feat: create task interface

--- pages/home.tsx
+++ pages/home.tsx
@@ -1,10 +1,16 @@
 import type { NextPage } from 'next'
-import { useQuery } from '@apollo/client'
+import { KeyboardEvent } from 'react'
+import { useQuery, useMutation } from '@apollo/client'
 
-import { GetTasksDocument } from '../graphql/dist/client'
-import { GetTasksQuery } from '../graphql/dist/client'
+import {
+  CreateTaskMutation,
+  CreateTaskDocument,
+  GetTasksQuery,
+  GetTasksDocument,
+} from '../graphql/dist/client'
 import { useSession } from 'next-auth/react'
 import { ContentHeader, Header } from '../components/header'
+import { useState } from 'react'
 
 const Home: NextPage = () => {
   const { data: session, status } = useSession()
@@ -12,15 +18,40 @@ const Home: NextPage = () => {
     variables: { userId: session?.user.id },
     skip: status === 'loading',
   })
+  const [inputValue, setInputValue] = useState('')
+  const [createTask, { data: createdTask, error, loading }] =
+    useMutation<CreateTaskMutation>(CreateTaskDocument)
+
+  const handleKeyDown = (event: KeyboardEvent<HTMLInputElement>) => {
+    if (event.key === 'Enter') {
+      createTask({
+        variables: {
+          task: {
+            name: inputValue,
+            userId: session?.user.id,
+          },
+        },
+      })
+    }
+  }
+
   return (
     <div style={{ margin: '0 auto', width: '1600px' }}>
       <Header></Header>
       <ContentHeader></ContentHeader>
+      <div>
+        <input
+          type="text"
+          value={inputValue}
+          placeholder="add new task"
+          onChange={(e) => setInputValue(e.target.value)}
+          onKeyDown={handleKeyDown}
+        ></input>
+      </div>
       {data?.tasks?.map((task) => (
         <div key={task.id}>
-          <h1>{task.content}</h1>
+          <h1>{task.name}</h1>
           <p>id:{task.id}</p>
-          <p>name:{task.name}</p>
           <p>content:{task.content}</p>
         </div>
       ))}
