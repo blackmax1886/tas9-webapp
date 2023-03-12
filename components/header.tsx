2d0fa0e27cdaf97cfa9e25f65c87521c75205261
blackmax <blackmax1886@gmail.com>
2023-03-12 14:17:36 +0900

    Feat: add sign out button

--- components/header.tsx
+++ components/header.tsx
@@ -1,21 +1,36 @@
 import { css } from '@emotion/react'
+import { signOut } from 'next-auth/react'
 
 const header = css`
   display: flex;
-  justify-content: space-between;
   align-items: center;
   padding: 1rem;
 `
 
 const logo = css`
-  font-size: 1.5rem;
+  font-size: 2rem;
+`
+
+const nav = css`
+  margin-left: auto;
+  display: flex;
+`
+
+const navItem = css`
+  font-size: 1rem;
+  padding: 1rem;
 `
 
 const Header = () => {
   return (
     <div css={header}>
       <div css={logo}>Tas9</div>
-      <div>Account</div>
+      <nav css={nav}>
+        <button onClick={() => signOut()} css={navItem}>
+          Sign Out
+        </button>
+        <div css={navItem}>Account</div>
+      </nav>
     </div>
   )
 }
