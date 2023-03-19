7688477ab56c32dae8e00120bf281d178e165fb0
blackmax <blackmax1886@gmail.com>
2023-03-19 19:02:34 +0900

    Feat: tasks view on timetable

--- components/task_card.tsx
+++ components/task_card.tsx
@@ -177,4 +177,142 @@ const TaskCards = ({
   )
 }
 
-export { TaskCard, TaskCards }
+const MovableTaskCard = ({
+  task,
+  refetch,
+}: {
+  task: Partial<Task> | undefined
+  refetch: QueryResult<GetTasksQuery>['refetch']
+}) => {
+  const [isDone, setIsDone] = useState(task?.done)
+  const [updateTaskIsDone] = useMutation<UpdateTaskIsDoneMutation>(
+    UpdateTaskIsDoneDocument,
+    {
+      onCompleted() {
+        refetch()
+      },
+    }
+  )
+  const [deleteTask] = useMutation<DeleteTaskMutation>(DeleteTaskDocument, {
+    onCompleted() {
+      refetch()
+    },
+  })
+
+  const handleTaskIsDone = () => {
+    setIsDone(!isDone)
+    updateTaskIsDone({
+      variables: {
+        taskId: task?.id,
+        isDone: !isDone,
+      },
+    })
+  }
+
+  const handleDeleteTask = () => {
+    deleteTask({ variables: { taskId: task?.id } })
+  }
+
+  const taskCard = css`
+    display: flex;
+    font-size: 1.5rem;
+    align-items: center;
+    border: 2px solid white;
+  `
+  const checkbox = css`
+    display: none;
+  `
+
+  const checkboxWrapper = css`
+    display: flex;
+    margin: 1rem;
+    position: relative;
+    width: 1.5rem;
+    align-items: center;
+  `
+
+  const checkboxWrapperLabel = css`
+    background: none repeat scroll 0 0 #eeeeee;
+    border: 1px solid #dddddd;
+    border-radius: 50%;
+    cursor: pointer;
+    height: 1.5rem;
+    width: 1.5rem;
+
+    &:after {
+      border: 2px solid #fff;
+      border-top: none;
+      border-right: none;
+      content: '';
+      height: 6px;
+      left: 0.4rem;
+      opacity: 0;
+      position: absolute;
+      top: 0.5rem;
+      transform: rotate(-45deg);
+      width: 12px;
+    }
+
+    ${isDone &&
+    css`
+      background-color: #66bb6a;
+      border-color: #66bb6a;
+      &:after {
+        opacity: 1;
+      }
+    `}
+  `
+
+  const taskLabel = css`
+    flex: 1 0 auto;
+    max-width: 80%;
+    overflow-wrap: break-word;
+  `
+
+  const deleteButton = css`
+    background: transparent;
+    border: none;
+  `
+
+  return (
+    <>
+      <div key={task?.id} css={taskCard}>
+        <div css={checkboxWrapper}>
+          <input type="checkbox" css={checkbox}></input>
+          <label css={checkboxWrapperLabel} onClick={handleTaskIsDone}></label>
+        </div>
+        <label css={taskLabel}>{task?.name}</label>
+        <button css={deleteButton} onClick={handleDeleteTask}>
+          <Image
+            src="/delete-button.png"
+            alt="delete"
+            height={24}
+            width={24}
+          ></Image>
+        </button>
+      </div>
+    </>
+  )
+}
+
+const MovableTaskCards = ({
+  data,
+  refetch,
+}: {
+  data: GetTasksQuery | undefined
+  refetch: QueryResult<GetTasksQuery>['refetch']
+}) => {
+  return (
+    <>
+      {data?.tasks.map((task: Partial<Task>) => (
+        <MovableTaskCard
+          key={task.id}
+          task={task}
+          refetch={refetch}
+        ></MovableTaskCard>
+      ))}
+    </>
+  )
+}
+
+export { TaskCard, TaskCards, MovableTaskCard, MovableTaskCards }
--- pages/timetable.tsx
+++ pages/timetable.tsx
@@ -1,11 +1,79 @@
+import { KeyboardEvent, useEffect, useState } from 'react'
+import { useQuery, useMutation } from '@apollo/client'
+import {
+  CreateTaskMutation,
+  CreateTaskDocument,
+  GetTasksQuery,
+  GetTasksDocument,
+} from '../graphql/dist/client'
+import { useSession } from 'next-auth/react'
+import { ContentHeader, Header } from '../components/header'
 import Board from '@/components/board'
-import { ContentHeader, Header } from '@/components/header'
+import { css } from '@emotion/react'
+import { MovableTaskCards } from '@/components/task_card'
+
+const container = css`
+  display: flex;
+  min-height: 800px;
+`
+const addTask = css`
+  box-sizing: border-box;
+  margin-bottom: 3px;
+  padding: 12px 16px;
+  border-color: transparent;
+  border-radius: 2px;
+  background-color: rgba(26, 24, 29, 0.06);
+  width: 100%;
+  font-size: inherit;
+`
 
 const TimeTable = () => {
+  const { data: session, status } = useSession()
+  const { data, refetch } = useQuery<GetTasksQuery>(GetTasksDocument, {
+    variables: { userId: session?.user.id },
+    skip: status === 'loading',
+  })
+  //TODO: rename inputValue
+  const [inputValue, setInputValue] = useState('')
+  const [createTask] = useMutation<CreateTaskMutation>(CreateTaskDocument, {
+    onCompleted() {
+      refetch()
+      setInputValue('')
+    },
+  })
+
+  const handleKeyDown = (event: KeyboardEvent<HTMLInputElement>) => {
+    if (event.key === 'Enter') {
+      createTask({
+        variables: {
+          task: {
+            name: inputValue,
+            userId: session?.user.id,
+          },
+        },
+      })
+    }
+  }
+
   return (
     <div style={{ margin: '0 auto', width: '1600px' }}>
       <Header></Header>
       <ContentHeader selected="Timetable"></ContentHeader>
+      <div css={container}>
+        <Board>
+          <div>
+            <input
+              type="text"
+              value={inputValue}
+              placeholder="+ add new task"
+              onChange={(e) => setInputValue(e.target.value)}
+              onKeyDown={handleKeyDown}
+              css={addTask}
+            ></input>
+          </div>
+          <MovableTaskCards data={data} refetch={refetch}></MovableTaskCards>
+        </Board>
+      </div>
     </div>
   )
 }
