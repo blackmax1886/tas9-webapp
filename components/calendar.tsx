719d003e637560e98a6450fc969915ada8777c59
blackmax <blackmax1886@gmail.com>
2023-03-21 18:47:57 +0900

    Feat: link tasks to events on calendar

--- components/calendar.tsx
+++ components/calendar.tsx
@@ -4,17 +4,47 @@ import {
   stringOrDate,
 } from 'react-big-calendar'
 import dayjs from 'dayjs'
+import 'dayjs/locale/ja'
 import 'react-big-calendar/lib/css/react-big-calendar.css'
 import withDragAndDrop from 'react-big-calendar/lib/addons/dragAndDrop'
 import { css } from '@emotion/react'
 import { Task } from '@/graphql/dist/client'
 
+dayjs.locale('ja')
+const formatString = 'YYYY-MM-DD HH:mm'
 const localizer = dayjsLocalizer(dayjs)
 const DragAndDropCalendar = withDragAndDrop(ReactBigCalendar)
+
+const tasksAsEvents = (tasks: Partial<Task>[] | undefined) => {
+  if (!tasks) {
+    return []
+  }
+  const events = tasks.map((task) => {
+    return {
+      title: task.name,
+      start: dayjs(task.start, formatString).toDate(),
+      end: dayjs(task.end, formatString).toDate(),
+    }
+  })
+  return events
+}
 
 const calendar = css`
   height: 800px;
 `
+
+const timeslot = css`
+  height: 50px;
+`
+
+type TimeSlotWrapperProps = {
+  children: React.ReactNode
+}
+
+// TODO: Fix type error from TimeSlowWrapper arg
+const TimeSlotWrapper: React.FC<TimeSlotWrapperProps> = ({ children }) => {
+  return <div css={timeslot}>{children}</div>
+}
 
 const Calendar = ({
   tasks,
@@ -29,13 +59,17 @@ const Calendar = ({
     end: stringOrDate
   }) => void
 }) => {
+  const events = tasksAsEvents(tasks)
   return (
     <div>
       <DragAndDropCalendar
         localizer={localizer}
-        //   events={myEventsList}
+        events={events}
         defaultView="day"
         onDropFromOutside={onDropFromOutside}
+        components={{
+          timeSlotWrapper: TimeSlotWrapper,
+        }}
         css={calendar}
       />
     </div>
